0fac571ae467c419e28eecc1b10f99c3a0df268c
doannn <doan.nguyenngoc950@gmail.com>
2022-08-15 17:30:54 +0700

    analysis problem

---
+++ leetcode/DataStructure/src/main/java/stack/_150_Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,25 @@
+package stack;
+
+
+/*          150. Evaluate Reverse Polish Notation
++ Phan tich bai toan:
+    - Reserve Polish Notation: có thể hiểu là cách thức biểu diễn trình tự tính toán. Nó tuân theo quy tắc:
+        - Syntax: {number1, number2, operation}
+        - Reference: https://vi.wikipedia.org/wiki/K%C3%BD_ph%C3%A1p_ngh%E1%BB%8Bch_%C4%91%E1%BA%A3o_Ba_Lan
+    - Input (String[]): chuỗi các kí tự của 1 biểu thức
+    - Output (int): kết quả biểu thức
+    - Lưu ý:
+        - Giả sử chuỗi kí tự đầu vào luôn hợp lệ & k xảy ra hiện tượng chia cho 0
+        - Phép chia 2 số nguyên sẽ được làm tròn 0.
++ Idea:
+    -
+*
+* */
+
+
+
+public class _150_Evaluate_Reverse_Polish_Notation {
+    public int evalRPN(String[] tokens) {
+        return 0;
+    }
+}
